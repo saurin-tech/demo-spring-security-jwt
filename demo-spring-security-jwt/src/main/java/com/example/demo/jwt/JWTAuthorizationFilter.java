79c0f88134b60e7073dc9b7a629b8fc305024723
Saurin Patel <saurin.tech@gmail.com>
2017-10-17 09:09:56 -0400

    Fixed the issue with parsing the token.  Got help from Bruno S. Krebs.

--- demo-spring-security-jwt/src/main/java/com/example/demo/jwt/JWTAuthorizationFilter.java
+++ demo-spring-security-jwt/src/main/java/com/example/demo/jwt/JWTAuthorizationFilter.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -47,17 +46,17 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter{
 		String token = request.getHeader(HEADER_STRING);
 		if(token != null) {
 			//parse the token
-			String user = Jwts.parser()
-					.setSigningKey(SECRET.getBytes())
-					.parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
-					.getBody()
-					.getSubject();
+//			String user = Jwts.parser()
+//					.setSigningKey(SECRET.getBytes())
+//					.parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+//					.getBody()
+//					.getSubject();
 			
 			Jws<Claims> claims = Jwts.parser()
-					  .setSigningKey("secret".getBytes())
-					  .parseClaimsJws(token);
+					  .setSigningKey(SECRET.getBytes())
+					  .parseClaimsJws(token.replace(TOKEN_PREFIX, ""));
 			
-			user = claims.getBody().getSubject();
+			String user = claims.getBody().getSubject();
 			System.out.println("=======> Printing the User in the GetAuthentication: " + user);	
 			String db = (String) claims.getBody().get("db");
 			System.out.println("=======> Printing the Db in the GetAuthentication: " + db);
